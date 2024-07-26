ce6ec753c27fa3c6d200d686a7a54d0633b6056f
Chris H. Meyer <christian.h.meyer@t-online.de>
2024-07-26 13:07:53 +0200

    Init

---
+++ app/src/main/java/de/cyberchrime/cycledcharge/MainActivity.kt
@@ -0,0 +1,69 @@
+    package de.cyberchrime.cycledcharge
+
+import android.content.Context
+import android.os.BatteryManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import de.cyberchrime.cycledcharge.ui.theme.CycledChargeTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+    class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        lifecycleScope.launch {
+            while (true) {
+                // Get the BatteryManager instance
+                val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+
+                // Retrieve the charging current in microamperes (uA)
+                val chargingCurrent = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+
+                // Convert microamperes to milliamperes
+                val chargingCurrentInmA = chargingCurrent / 1000
+
+                setContent {
+                    CycledChargeTheme {
+                        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                            CurrentPrint(
+                                currentInmA = "$chargingCurrentInmA",
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                        }
+                    }
+                }
+
+                delay(1000)
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrentPrint(currentInmA: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Current: $currentInmA mA",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CycledChargeTheme {
+        CurrentPrint("0")
+    }
+}
